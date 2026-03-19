7f4a4854762fd0306d291c3ed648d8ade6a44e63
matheustorrens <matheus.cacemiro@gmail.com>
2026-03-18 22:21:48 -0300

    Security: move Firebase credentials to env vars

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, setPersistence, browserSessionPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-jmzZFnkK9l763pz3pDFTgPWqGQxZnTE",
-  authDomain: "barbearia-guaruja-01.firebaseapp.com",
-  projectId: "barbearia-guaruja-01",
-  storageBucket: "barbearia-guaruja-01.firebasestorage.app",
-  messagingSenderId: "36769301956",
-  appId: "1:36769301956:web:b27391d8df3d08cd1bd091",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
